2b39002ad1905222ad9077f9457b01dfd0349cfc
sahadahmed995 <sac903299@gmail.com>
2024-01-04 18:31:45 +0600

    compliteBanner

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,6 +1,19 @@
 $(function(){
+
+    // *nav starts
     $(".navbar-nav li").click(function(){
-        $(".navbar-nav li").removeClass("active")
-        $(this).addClass("active")
+        $(this).siblings().removeClass("active")
+        $(this).toggleClass("active")
     })
+    // *nav ends
+
+    // *banner slider starts
+    $(".bannerSlider").slick({
+        arrows: false,
+        dots: true,
+        dotsClass: "container slick-dots",
+        autoplay: true,
+        autoplaySpeed: 3000,
+    })
+    // *banner slider ends
 })
