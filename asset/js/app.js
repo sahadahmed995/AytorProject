bbf53dbb1f156f031e22945c26ce8e1494caea3d
sahadahmed995 <sac903299@gmail.com>
2024-01-18 00:59:03 +0600

    latestNewsComplite

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,8 +1,8 @@
 $(function(){
 
-  // aos starts
+  // *aos starts
   AOS.init();
-  // aos ends
+  // *aos ends
 
     // *nav starts
     $(".navbar-nav li").click(function(){
@@ -52,7 +52,7 @@ $(function(){
     })
     // *NEW product ends
 
-    // deal fo the day starts
+    // *deal fo the day starts
     $(".dealSlider").slick({
       slidesToShow: 2,
       slidesToScroll: 2,
@@ -72,14 +72,53 @@ $(function(){
     });
 
 
-    // Deal Of The Day CountDown Starts
+    // *Deal Of The Day CountDown Starts
     $('.timer').countdown('2024/02/10', function(event) {
       $('.timer .sec').html(event.strftime('%S'));
       $('.timer .min').html(event.strftime('%M'));
       $('.timer .hour').html(event.strftime('%H'));
       $('.timer .day').html(event.strftime('%D'));
     })
-    // Deal Of The Day CountDown Ends 
-    
-    // deal fo the day ends
+    // *Deal Of The Day CountDown Ends 
+    new VenoBox({
+      selector: '.popUpBox',
+      spinner: 'rotating-plane'
+  });
+    // *deal fo the day ends
+
+    // *latest news starts
+    $(".latestSlider").slick({
+      slidesToShow: 4,
+      slidesToScroll: 2,
+      arrows: false,
+      dots: true,
+      autoplay:true,
+      autoplaySpeed:2500,
+      speed:1000,
+      responsive: [
+        {
+          breakpoint: 992,
+          settings:{
+            slidesToShow: 2,
+            slidesToScroll:1,
+          },
+        },
+        {
+          breakpoint: 767,
+          settings:{
+            slidesToShow: 1,
+            slidesToScroll:1,
+          },
+        },
+        {
+          breakpoint: 575,
+          settings:{
+            slidesToShow: 1,
+            slidesToScroll:1,
+            dots:false,
+          },
+        },
+      ]
+    })
+    // *latest news ends
 })
